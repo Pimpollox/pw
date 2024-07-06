4c1802982a29bc891448d1abe3ed7f65e67fb232
helard0 <87501145+helard0@users.noreply.github.com>
2024-07-06 14:36:15 -0500

    Update ProductCard.jsx

--- Front-G1/src/pages/ProductCard.jsx
+++ Front-G1/src/pages/ProductCard.jsx
@@ -1,21 +1,17 @@
-/* eslint-disable no-unused-vars */
-import React, {useContext} from 'react';
-import PropTypes from 'prop-types'; // Importa PropTypes
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import CartContext from '../CartContext';
-function ProductCard({ product, brand }) {
 
-    const { addToCart } = useContext(CartContext);
+function ProductCard({ product, brand }) {
+  const { addToCart } = useContext(CartContext);
   return (
     <div className="product-card">
-        <div className="img-container"><img src={product.imagen} alt={product.Serie} /></div>
-      
+      <div className="img-container"><img src={product.imagen} alt={product.Serie} /></div>
       <h3>{product.Serie}</h3>
       <h4>{brand}</h4>
       <p className="price">S/. {product.precio}</p>
-      <button className="btn" onClick={() => addToCart(product)}> 
-        Agregar al Carrito
-      </button>
+      <button className="btn" onClick={() => addToCart(product)}>Agregar al Carrito</button>
       <div className='DetailLink'><Link to={`/producto/${product.Serie}`}>Ver más</Link></div> 
     </div>
   );
@@ -25,7 +21,7 @@ ProductCard.propTypes = {
   product: PropTypes.shape({
     imagen: PropTypes.string.isRequired,
     Serie: PropTypes.string.isRequired,
-    precio: PropTypes.number.isRequired, 
+    precio: PropTypes.number.isRequired,
   }).isRequired,
   brand: PropTypes.string.isRequired
 };
