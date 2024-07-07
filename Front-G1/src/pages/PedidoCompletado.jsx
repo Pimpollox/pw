5c006cf99e1be5a13e2b653c9cc08b93f0c00e21
helard0 <87501145+helard0@users.noreply.github.com>
2024-07-07 01:50:17 -0500

    Update PedidoCompletado.jsx

--- Front-G1/src/pages/PedidoCompletado.jsx
+++ Front-G1/src/pages/PedidoCompletado.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import ProductCard from './ProductCard';
-import './PedidoCompletado.css'
+import './PedidoCompletado.css';
 import HeaderPrincipal from '../componentes/HeaderPrincipal';
 import FooterPrincipal from '../componentes/FooterPrincipal';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function PedidoCompletado() {
+  const [productosInteres, setProductosInteres] = useState([]);
 
-    return (
-        <>
-        <HeaderPrincipal />
-        <div id="PedidoC">
-            <div id="Aviso1">
-                <p>¡Muchas gracias por tu pedido!</p>
-            </div>
-            <div id="Aviso2">
-                <p>Puedes ver los detalles de tu pedido ingresando a <u><a><Link to="/ordRecientes">tu cuenta</Link></a></u></p>
-            </div>
-            <div id="Aviso3">
-                <p>Tambien te podria interesar...</p>
-            </div>
-            <div className="ProductosI">
-                {productosInteres.map(producto => (
+  useEffect(() => {
+    fetch('http://localhost/api/modelos')
+      .then(response => response.json())
+      .then(data => {
+        setProductosInteres(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []); 
+  return (
+    <>
+      <HeaderPrincipal />
+      <div id="PedidoC">
+        <div id="Aviso1">
+          <p>¡Muchas gracias por tu pedido!</p>
+        </div>
+        <div id="Aviso2">
+          <p>Puedes ver los detalles de tu pedido ingresando a <u><Link to="/ordRecientes">tu cuenta</Link></u></p>
+        </div>
+        <div id="Aviso3">
+          <p>También te podría interesar...</p>
+        </div>
+        <div className="ProductosI">
+          {productosInteres.map(producto => (
             <ProductCard key={producto.id} product={producto} brand={producto.marca} />
           ))}
-            </div>
         </div>
-        <FooterPrincipal />
-        </>
-    )
-    
+      </div>
+      <FooterPrincipal />
+    </>
+  );
 }
 
-export default PedidoCompletado
+export default PedidoCompletado;
